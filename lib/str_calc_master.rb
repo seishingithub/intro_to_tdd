9be177e1ba70d78275512e18f4b34e43eed72672
Peggy Griffin <seishingithub@gmail.com>
2014-03-17 08:44:34 -0600

    specs return 0 on empty string & returns sum of #s on StringCalcMaster

---
+++ lib/str_calc_master.rb
@@ -0,0 +1,7 @@
+class StringCalcMaster
+  def add(str)
+    str.to_i
+  end
+
+end
+
---
+++ spec/str_calc_master_spec.rb
@@ -0,0 +1,32 @@
+=begin
+
+else returns sum of nums in string
+e.g. "1" returns 1
+e.g. "1,2,7" return 10
+
+The delimiter (",") can be specified at initialization time.
+Any calls to add will use that delimiter.
+=end
+
+require 'str_calc_master'
+
+describe StringCalcMaster do
+  it "returns 0 if there is an empty string" do
+    mini_string = StringCalcMaster.new
+
+    expected = 0
+    actual = mini_string.add(" ")
+
+    expect(actual).to eq expected
+  end
+
+  it "returns sum of numbers in a string" do
+    mini_string = StringCalcMaster.new
+
+    expected = 1
+    actual = mini_string.add("1")
+
+    expect(actual).to eq expected
+  end
+end
+
